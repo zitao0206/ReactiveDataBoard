86815a299354031294dda72d2fd0cf4cb43485d6
Leo <zitao.li@quvideo.com>
2018-09-02 17:57:55 +0800

    数据白板XYReactWhiteBoard支持自动释放功能；

--- XYReactDataBoard/Classes/XYReactWhiteBoard.h
+++ XYReactDataBoard/Classes/XYReactWhiteBoard.h
@@ -8,19 +8,27 @@
 #import <Foundation/Foundation.h>
 #import <ReactiveObjC/ReactiveObjC.h>
 
+#define XY_KEYPATH(key) [NSString stringWithFormat:@"%@ %@",key, self]
+
 @interface XYReactWhiteBoard : NSObject
 
 + (nonnull instancetype)shareBoard;
-
 /**
  *  订阅制定key的变化通知，支持初始值获取；
  *
- *  @param key 不允许空值， 建议使用keypath宏声称键值字符串
+ *  @param key 不允许空值， 建议使用XY_KEYPATH宏声称键值字符串
  *
  *  @return signal
  */
 - (nonnull RACSignal *)signalForKey:(nonnull NSString *)key;
-
+/**
+ *  订阅制定key的变化通知，支持初始值获取；
+ *
+ *  @param keyPath 不允许空值， 须使用XY_KEYPATH宏声称键值字符串
+ *
+ *  @return signal
+ */
+- (nonnull RACSignal *)signalForKeyPath:(nonnull NSString *)keyPath;
 /**
  *  订阅制定key的变化通知，支持初始值获取，首次订阅有效，无法被覆盖；
  *
@@ -29,7 +37,14 @@
  *  @return signal
  */
 - (nonnull RACSignal *)singleSignalForKey:(nonnull NSString *)key;
-
+/**
+ *  订阅制定key的变化通知，支持初始值获取，首次订阅有效，无法被覆盖；
+ *
+ *  @param keyPath 不允许空值， 须使用XY_KEYPATH宏声称键值字符串
+ *
+ *  @return signal
+ */
+- (nonnull RACSignal *)singleSignalForKeyPath:(nonnull NSString *)keyPath;
 /**
  *  数据更新
  *
@@ -37,16 +52,14 @@
  *  @param key   不允许为空
  */
 - (void)setValue:(nullable id)value forKey:(nonnull NSString *)key;
-
 /**
  *  同步获取特定值；
  *
- *  @param key 建议使用keypath宏声称键值字符串
+ *  @param key 建议使用XY_KEYPATH宏声称键值字符串
  *
  *  @return value
  */
 - (nullable id)valueForKey:(nonnull NSString *)key;
-
 /**
  *  删除指定key和value；
  */
--- XYReactDataBoard/Classes/XYReactWhiteBoard.m
+++ XYReactDataBoard/Classes/XYReactWhiteBoard.m
@@ -7,10 +7,14 @@
 
 #import "XYReactWhiteBoard.h"
 #import "XYReactDataBoardSubject.h"
-
 @interface XYReactWhiteBoard ()
 @property (nonatomic, strong) NSMutableDictionary *subjects;
 @property (nonatomic, strong) NSMutableDictionary *values;
+@property (nonatomic, strong) NSMapTable *mapTable;
+@property (nonatomic, strong) NSMutableDictionary *keyPaths;
+@property (nonatomic, strong) dispatch_source_t dispatchSource;
+@property (nonatomic, assign) BOOL isWatching;
+
 @end
 
 @implementation XYReactWhiteBoard
@@ -44,16 +48,22 @@ static dispatch_once_t onceToken;
 - (void)setValue:(id)value forKey:(NSString *)key
 {
     if (key.length <= 0) return;
-    if (value) {
-        @synchronized (self) {
+    @synchronized (self) {
+        if (value) {
             [self.values setObject:value forKey:key];
-        }
-    } else {
-        @synchronized (self) {
+        } else {
             [self.values removeObjectForKey:key];
         }
+        if (self.keyPaths.count > 0) {
+            for (NSString *keyPath in self.keyPaths.allKeys) {
+                NSArray *array = [keyPath componentsSeparatedByString:@" "];
+                if ([array.firstObject isEqualToString:key]) {
+                    [[self subjectForKey:keyPath] sendNext:value];
+                }
+            }
+        }
+        [[self subjectForKey:key] sendNext:value];
     }
-    [[self subjectForKey:key] sendNext:value];
 }
 
 - (id)valueForKey:(NSString *)key
@@ -69,6 +79,23 @@ static dispatch_once_t onceToken;
         subject = [RACSubject subject];
         @synchronized (self) {
             [self.subjects setObject:subject forKey:key];
+            [self.rac_deallocDisposable addDisposable:[RACDisposable disposableWithBlock:^{
+                [subject sendCompleted];
+            }]];
+        }
+    }
+    return subject;
+}
+
+- (RACSignal *)signalForKeyPath:(NSString *)keyPath
+{
+    [self handleKeyPath:keyPath];
+    if (keyPath.length <= 0) return [RACSignal empty];
+    RACSubject *subject = [self subjectForKey:keyPath];
+    if (subject == nil) {
+        subject = [RACSubject subject];
+        @synchronized (self) {
+            [self.subjects setObject:subject forKey:keyPath];
             [self.rac_deallocDisposable addDisposable:[RACDisposable disposableWithBlock:^{
                 [subject sendCompleted];
             }]];
@@ -93,9 +120,63 @@ static dispatch_once_t onceToken;
     return subject;
 }
 
+- (nonnull RACSignal *)singleSignalForKeyPath:(nonnull NSString *)keyPath
+{
+    [self handleKeyPath:keyPath];
+    if (keyPath.length <= 0) return [RACSignal empty];
+    RACSubject *subject = [self subjectForKey:keyPath];
+    if (subject == nil || ![subject isKindOfClass:[XYReactDataBoardSubject class]]) {
+        subject = [XYReactDataBoardSubject subject];
+        @synchronized (self) {
+            [self.subjects setObject:subject forKey:keyPath];
+            [self.rac_deallocDisposable addDisposable:[RACDisposable disposableWithBlock:^{
+                [subject sendCompleted];
+            }]];
+        }
+    }
+    return subject;
+}
+
 - (RACSubject *)subjectForKey:(NSString *)key
 {
     return [self.subjects objectForKey:key];
+}
+
+#pragma mark - memory address handle
+
+- (NSString *)handleKeyPath:(NSString *)keyPath
+{
+    NSArray *array = [keyPath componentsSeparatedByString:@" "];
+    NSString *ptr = array.lastObject;
+    if (ptr.length > 0) {
+        ptr = [ptr hasPrefix:@"0x"] ? ptr : [@"0x" stringByAppendingString:ptr];
+        unsigned long long hex = strtoull(ptr.UTF8String, NULL, 0);
+        id obj = (__bridge id)(void *)hex;
+        if (obj) {
+            [self.mapTable setObject:obj forKey:keyPath];
+            [self.keyPaths setObject:keyPath forKey:keyPath];
+        }
+    }
+    if (!self.isWatching && self.keyPaths.count > 0) {
+        self.isWatching = YES;
+        [self startWatch];
+    }
+    return array.firstObject;
+}
+
+- (void)visitAllElements
+{
+    NSLog(@"数据白板keyPath：%@", self.mapTable);
+    for (NSString *keyPath in self.keyPaths.allKeys) {
+        id obj = [self.mapTable objectForKey:keyPath];
+        if (!obj) {
+            @synchronized (self) {
+                [self.keyPaths removeObjectForKey:keyPath];
+                [self.values removeObjectForKey:keyPath];
+                [self.subjects removeObjectForKey:keyPath];
+            }
+        }
+    }
 }
 
 #pragma mark - accessor
@@ -116,13 +197,58 @@ static dispatch_once_t onceToken;
     return _values;
 }
 
+- (NSMapTable *)mapTable
+{
+    if (nil == _mapTable) {
+       _mapTable = [NSMapTable mapTableWithKeyOptions:NSMapTableWeakMemory valueOptions:NSMapTableWeakMemory];
+    }
+    return _mapTable;
+}
+
+- (dispatch_source_t)dispatchSource
+{
+    if (nil == _dispatchSource) {
+        dispatch_queue_t dispatchQueue = dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT);
+        _dispatchSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatchQueue);
+        dispatch_source_set_timer(_dispatchSource,
+                                  dispatch_time(DISPATCH_TIME_NOW, 0),
+                                  NSEC_PER_SEC,
+                                  0);
+        dispatch_source_set_event_handler(_dispatchSource, ^{
+            [self visitAllElements];
+            if (self.subjects.count == 0) {
+                [self suspendWatch];
+                self.isWatching = NO;
+            }
+        });
+    }
+    return _dispatchSource;
+}
+
+- (NSMutableDictionary *)keyPaths
+{
+    if (nil == _keyPaths) {
+        _keyPaths = [NSMutableDictionary dictionary];
+    }
+    return _keyPaths;
+}
+
+- (void)startWatch
+{
+    dispatch_resume(self.dispatchSource);
+}
+
+- (void)suspendWatch
+{
+    dispatch_suspend(self.dispatchSource);
+}
+
 #pragma mark - remove and destroy
 
 - (void)removeValueForKey:(nonnull NSString *)key
 {
     if (key.length <= 0) return;
     @synchronized (self) {
-        [self.subjects removeObjectForKey:key];
         [self.values removeObjectForKey:key];
     }
 }
